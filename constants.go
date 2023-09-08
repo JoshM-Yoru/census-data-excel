ed98f23c84b7c97df9ba8f230c14b1d9a4d2c517
JoshM-Yoru <jmaciel335i@gmail.com>
2023-09-08 17:05:40 -0400

    Added constants

---
+++ constants.go
@@ -0,0 +1,8 @@
+package main
+
+
+const CENSUS_API_URL = "https://geocoding.geo.census.gov";
+
+const BENCHMARKS = "Public_AR_Current";
+
+const VINTAGES = "Current_Current";
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+
+    fmt.Println(CENSUS_API_URL);
+
+}
